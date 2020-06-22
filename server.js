0b457966cf95dc384105acbd88015dd71c3513b4
mickyngub <micky47561@gmail.com>
2020-06-22 09:43:55 +0000

    default setup

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/exercise-track' , { useNewUrlParser: true, useUnifiedTopology: true })
+
+let db = mongoose.connection;
+db.on('error', console.error.bind("connection error"));
+db.once('open', () => console.log("we're in!"));
 
 app.use(cors())
 
@@ -14,15 +18,24 @@ app.use(bodyParser.json())
 
 
 app.use(express.static('public'))
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+})
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
 
 // Not found middleware
+
+
 app.use((req, res, next) => {
   return next({status: 404, message: 'not found'})
 })
+
 
 // Error Handling middleware
 app.use((err, req, res, next) => {
