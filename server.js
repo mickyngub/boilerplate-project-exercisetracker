0297f2f786cda7f996ca8c3482942e8fac66e6d6
mickyngub <micky47561@gmail.com>
2020-06-25 16:13:30 +0000

    everything is finally working(problem with date?)

--- server.js
+++ server.js
@@ -62,11 +62,21 @@ let personSchema = new mongoose.Schema({
 
   exercise: [{ description: {type: String, default: ""},
               duration: {type: Number, default: 0},
-              date: {type : Date, default: Date.now} }]
+              date: {type : Date, default: Date.now()} }]
   
 });
 
 let Person = mongoose.model("Person", personSchema);
+
+let getUsernameById = async (id, done) => {
+  Person.find({_id: id}, (err, people) => {
+    if(err) {
+      return console.error(err);
+    }
+    done(null,people);
+  });
+
+};
 
 const createPerson = (name, done) => {
   let objPerson = new Person({
@@ -86,7 +96,7 @@ const createPerson = (name, done) => {
 var findPersonAndUpdate = (id, 
     Ndescription = "blank",
   Nduration = 0,
-  Ndate = Date.now,
+  Ndate,
   done) => {
     let updater = { description : Ndescription, duration: Nduration, date: Ndate};
     Person.findOneAndUpdate(
@@ -142,15 +152,40 @@ app.post("/api/exercise/add", (req, res, done) => {
   }).then(result => {
     console.log(`Does this ID exists? ${result}`);
     if(result) {
-      console.log("We is here");
+      let { userId, description, duration, date } = req.body;
+      let testRegex = /\d{4}_\d{2}_\d{2}/;
+      let testRegex2 = /[a-zA-z]{1,}/;
+      if(testRegex2.test(date)) {
+        console.log("THIS IS NOT RIGHT");
+        date = '';
+      }
+      
+      let dateObj = date === '' ? new Date() : new Date(date);
+     
+     // dateObj = dateObj.toString().slice(0,15);
+      req.body.date = dateObj;
+      console.log(dateObj);
+      console.log("We are here");
       findPersonAndUpdate(req.body.userId, 
       req.body.description,
       req.body.duration,
-      req.body.date,
+      dateObj,
       (err, people) => {
         if(err) return console.error(err);
         console.log('We here');
-        res.json(people);
+        // res.json({'username': people.username, '_id':people._id, 'exercises':people.exercise});
+        // if(req.body.date == "") {
+        //   console.log("nullnullnull")
+        //   req.body.date = Date.now();
+        // }
+         let conUpdate = {
+            _id: userId,
+            description,
+            duration: +duration,
+            date: dateObj.toString().slice(0, 15),
+            username: people.username 
+          };
+        res.json(conUpdate);
         done(null, people);
       });
     } else {
@@ -163,6 +198,47 @@ app.post("/api/exercise/add", (req, res, done) => {
 app.get("/api/exercise/users", async (req, res, done) => {
   let listOfUsers = await Person.find({});
   res.json(listOfUsers.map( ({_id, username}) => ({_id, username})));
+})
+
+app.get("/api/exercise/log", async (req, res) => {
+  const { userId, from, to, limit } = req.query;
+  let listOfExercises = await Person.find({'_id':userId});
+ 
+  let user = listOfExercises[0].username;
+  listOfExercises = listOfExercises.map(({exercise}) => ({exercise}));
+  listOfExercises = listOfExercises[0];
+  console.log(listOfExercises);
+  listOfExercises.exercise = listOfExercises.exercise.map((exercise) => ({ description: exercise.description, duration: exercise.duration, date: exercise.date.toString().slice(0,15), _id: exercise.id, username: user}));
+  // console.log(listOfExercises);
+  // listOfExercises.exercise = listOfExercises.exercise.map(({obj}) => ({obj.date=obj.date.toString().slice(0,15)}));
+  console.log(listOfExercises);
+  // res.json(listOfExercises);
+  // if(from) {
+  //   const fromDate = new Date(from);
+  //   listOfExercises.exercise = listOfExercises.exercise.filter(exercise => 
+  //     new Date(exercise.date) >= fromDate 
+  //     );
+  // }
+  // if(to) {
+  //   const toDate = new Date(to);
+  //   listOfExercises.exercise = listOfExercises.exercise.filter(exercise => 
+  //     new Date(exercise.date) <= toDate
+  //     );
+  // }
+
+  if (from) {
+    const fromDate = new Date(from);
+    listOfExercises.exercise = listOfExercises.exercise.filter( exe => new Date(exe.date) >= fromDate);
+  }
+  
+  if (to) {
+    const toDate = new Date(to);
+    listOfExercises.exercise = listOfExercises.exercise.filter( exe => new Date(exe.date) <= toDate);
+  }
+  if(limit) {
+    listOfExercises.exercise = listOfExercises.exercise.slice(0, +limit);
+  }
+  await res.json({userId, username: user, count: listOfExercises.exercise.length, log: listOfExercises.exercise});
 })
  
 const listener = app.listen(8080, () => {
